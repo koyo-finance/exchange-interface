65dd229ebb6703a7efc57c63628e3c9fc3e73873
p7m <perpetuum7@protonmail.com>
2022-05-08 21:55:42 +0200

    feat: show pending transactions

--- src/hooks/contracts/StableSwap/useAddLiquidity.ts
+++ src/hooks/contracts/StableSwap/useAddLiquidity.ts
@@ -3,13 +3,23 @@ import { swapContracts } from 'core/contracts';
 import { ContractReceipt, Signer } from 'ethers';
 import { UseMutationResult } from 'react-query';
 import { StableSwap4Pool } from 'types/contracts/exchange';
+import { useAddRecentTransaction } from '@rainbow-me/rainbowkit';
 
 export default function useAddLiquidity(
 	signer: Signer | undefined,
 	swap: string
 ): UseMutationResult<ContractReceipt | undefined, unknown, Parameters<StableSwap4Pool['add_liquidity']>> {
+	const addRecentTransaction = useAddRecentTransaction();
+
 	const swapContract = swapContracts.get(swap);
 
-	const addLiquidity = useSmartContractTransaction(swapContract, 'add_liquidity', signer);
+	const addLiquidity = useSmartContractTransaction(swapContract, 'add_liquidity', signer, {
+		onTransactionSubmitted: (tx) => {
+			addRecentTransaction({
+				hash: tx.hash,
+				description: 'Adding liquidity.'
+			});
+		}
+	});
 	return addLiquidity;
 }
--- src/hooks/contracts/useSetTokenAllowance.ts
+++ src/hooks/contracts/useSetTokenAllowance.ts
@@ -2,13 +2,23 @@ import { ERC20Permit, ERC20Permit__factory } from '@elementfi/elf-council-typech
 import { useSmartContractTransaction } from '@elementfi/react-query-typechain';
 import { ContractReceipt, Signer } from 'ethers';
 import { UseMutationResult } from 'react-query';
+import { useAddRecentTransaction } from '@rainbow-me/rainbowkit';
 
 export default function useSetTokenAllowance(
 	signer: Signer | undefined,
 	tokenAddress: string
 ): UseMutationResult<ContractReceipt | undefined, unknown, Parameters<ERC20Permit['approve']>> {
+	const addRecentTransaction = useAddRecentTransaction();
+
 	const tokenContract: ERC20Permit | undefined = signer ? ERC20Permit__factory.connect(tokenAddress, signer) : undefined;
 
-	const approve = useSmartContractTransaction(tokenContract, 'approve', signer);
+	const approve = useSmartContractTransaction(tokenContract, 'approve', signer, {
+		onTransactionSubmitted: (tx) => {
+			addRecentTransaction({
+				hash: tx.hash,
+				description: 'Approving asset spending.'
+			});
+		}
+	});
 	return approve;
 }
