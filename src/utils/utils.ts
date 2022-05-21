fb0a452b90c4e6baa78e7036f0cbec750fd68f6c
p7m <perpetuum7@protonmail.com>
2022-05-21 11:02:02 +0200

    refactor: remove useless utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,4 @@
 import { CHAIN_INFO, SupportedChainId } from 'constants/chains';
-import { BigNumberish } from 'ethers';
-import { formatEther } from 'ethers/lib/utils';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export function noop() {}
-
-export function shortenHex(hex: string, length = 4) {
-	return `${hex.substring(0, length + 2)}…${hex.substring(hex.length - length)}`;
-}
 
 export enum ExplorerType {
 	Account,
@@ -32,19 +23,4 @@ export function formatExplorerLink(type: ExplorerType, data: ExplorerData) {
 			return `${CHAIN_INFO[chainId].explorer}/block/${block}`;
 		}
 	}
-}
-
-export function format18DecimalBalance(val: BigNumberish): string {
-	return Number(formatEther(val)).toLocaleString('fullwide', {
-		maximumFractionDigits: 5,
-		minimumFractionDigits: 2
-	});
-}
-
-export function calculate18DecimalPercentage(full: BigNumberish, part: BigNumberish): number {
-	const fullNumber = Number(formatEther(full));
-	const partNumber = Number(formatEther(part));
-
-	const result = (100 * partNumber) / fullNumber;
-	return Number.isNaN(result) ? 0 : result;
 }
