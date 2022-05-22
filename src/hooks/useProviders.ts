220a5e5b10cc017387ef6bcd6dff631930b67d29
p7m <perpetuum7@protonmail.com>
2022-05-22 10:38:35 +0200

    refactor: remove unused utils

--- src/hooks/useProviders.ts
+++ src/hooks/useProviders.ts
@@ -1,16 +1,12 @@
-import { AlchemyProvider, JsonRpcProvider, Provider } from '@ethersproject/providers';
-import { BOBA_MAINNET_READ_RPC_URL, BOBA_RINKEBY_RPC_URL, PinnedChainId, SupportedChainId } from 'constants/chains';
+import { JsonRpcProvider, Provider } from '@ethersproject/providers';
+import { BOBA_MAINNET_READ_RPC_URL, BOBA_RINKEBY_RPC_URL, SupportedChainId } from 'constants/chains';
 
 export const bobaProvider = new JsonRpcProvider(BOBA_MAINNET_READ_RPC_URL, SupportedChainId.BOBA);
 export const bobaRinkebyProvider = new JsonRpcProvider(BOBA_RINKEBY_RPC_URL, SupportedChainId.BOBA_RINKEBY);
 
-export const mainnetAlchemyProvider = new AlchemyProvider(PinnedChainId.MAINNET, process.env.NEXT_PUBLIC_MAINNET_KEY);
-
-export default function useProviders(): { [K in SupportedChainId | PinnedChainId]: Provider } {
+export default function useProviders(): { [K in SupportedChainId]: Provider } {
 	return {
 		[SupportedChainId.BOBA]: bobaProvider,
-		[SupportedChainId.BOBA_RINKEBY]: bobaRinkebyProvider,
-
-		[PinnedChainId.MAINNET]: mainnetAlchemyProvider
+		[SupportedChainId.BOBA_RINKEBY]: bobaRinkebyProvider
 	};
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
 
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
+
 					<link href="https://fonts.googleapis.com/css2?family=Sora:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;500;600;700;800&display=swap"
