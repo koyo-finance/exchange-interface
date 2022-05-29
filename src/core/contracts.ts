8f902594550d1e91bcab3a2ea151930d3c5dfca4
p7m <perpetuum7@protonmail.com>
2022-05-29 15:38:48 +0200

    feat: add minter hook

--- src/core/contracts.ts
+++ src/core/contracts.ts
@@ -1,9 +1,13 @@
 import { augmentedPools } from '@koyofinance/swap-sdk';
-import { BOBA_veKYO_ADDRESS } from 'constants/contracts';
+import { BOBA_KYO_MINTER_ADDRESS, BOBA_veKYO_ADDRESS } from 'constants/contracts';
 import { bobaReadonlyProvider } from 'hooks/useProviders';
 import { StableSwap4Pool, StableSwap4Pool__factory } from 'types/contracts/exchange';
-import { VotingEscrow__factory } from 'types/contracts/koyo';
+import { Minter__factory, VotingEscrow__factory } from 'types/contracts/koyo';
 
+export const kyoMinterContract = Minter__factory.connect(
+	BOBA_KYO_MINTER_ADDRESS, //
+	bobaReadonlyProvider
+);
 export const votingEscrowContract = VotingEscrow__factory.connect(
 	BOBA_veKYO_ADDRESS, //
 	bobaReadonlyProvider
---
+++ src/hooks/contracts/KYO/useMintAndDistribute.ts
@@ -0,0 +1,21 @@
+import { useSmartContractTransaction } from '@elementfi/react-query-typechain';
+import { useAddRecentTransaction } from '@rainbow-me/rainbowkit';
+import { kyoMinterContract } from 'core/contracts';
+import { ContractReceipt, Signer } from 'ethers';
+import { UseMutationResult } from 'react-query';
+import { Minter } from 'types/contracts/koyo';
+
+export function useMintAndDistribute(
+	signer: Signer | undefined
+): UseMutationResult<ContractReceipt | undefined, unknown, Parameters<Minter['mint_and_distribute']>> {
+	const addRecentTransaction = useAddRecentTransaction();
+
+	return useSmartContractTransaction(kyoMinterContract, 'mint_and_distribute', signer, {
+		onTransactionSubmitted: (tx) => {
+			addRecentTransaction({
+				hash: tx.hash,
+				description: 'Minting and distributing KYO.'
+			});
+		}
+	});
+}
