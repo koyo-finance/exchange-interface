c3eb74bb0e282106bd52ff1860b43ef62b37776f
encryptedDegen <105606904+encryptedDegen@users.noreply.github.com>
2022-05-31 18:45:56 +0200

    Unused import! Again!

--- src/hooks/contracts/KYO/useGetLockTimeEscrow.ts
+++ src/hooks/contracts/KYO/useGetLockTimeEscrow.ts
@@ -1,6 +1,6 @@
 import { useSmartContractReadCall } from '@elementfi/react-query-typechain';
 import { votingEscrowContract } from 'core/contracts';
-import { BigNumber, BigNumberish } from 'ethers';
+import { BigNumber } from 'ethers';
 import { QueryObserverResult } from 'react-query';
 
 export default function useGetLockTimeEscrow(account: string | null | undefined): QueryObserverResult<BigNumber> {
