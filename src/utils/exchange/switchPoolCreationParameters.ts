9d42be4d3439bac16b07990a6945fcc999761874
p7m <perpetuum7@protonmail.com>
2022-06-28 06:12:01 +0200

    fix: scale swap fee

--- src/utils/exchange/switchPoolCreationParameters.ts
+++ src/utils/exchange/switchPoolCreationParameters.ts
@@ -1,13 +1,16 @@
 import { BigNumber, BigNumberish } from 'ethers';
 import { PoolType } from 'state/reducers/createPool';
 
+const scalingFactor = BigNumber.from(10).pow(16);
+
 export function switchPoolCreationParameters(
 	poolType: PoolType,
 	weights: BigNumberish[] = [],
 	amplificationParameter: BigNumberish = BigNumber.from(0),
-	swapFeePercentage: BigNumberish = BigNumber.from(0)
+	swapFeePercentage: BigNumberish = 0
 ): [BigNumberish[], string[], BigNumberish] | [BigNumberish[], BigNumberish, boolean] | [BigNumberish, BigNumberish] {
-	weights = weights.map((w) => BigNumber.from(w).mul(BigNumber.from(10).pow(16)));
+	weights = weights.map((w) => BigNumber.from(w).mul(scalingFactor));
+	swapFeePercentage = BigNumber.from((swapFeePercentage as number) * 10000).mul(BigNumber.from(scalingFactor).div(10000));
 
 	switch (poolType) {
 		case PoolType.WEIGHTED: {
