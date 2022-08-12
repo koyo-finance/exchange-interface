c76604d3028ed913274119238de783bd7e8870cd
p7m <perpetuum7@protonmail.com>
2022-08-12 12:22:46 +0200

    feat: calculate swap using momiji

--- src/pages/swap/index.tsx
+++ src/pages/swap/index.tsx
@@ -27,6 +27,8 @@ import { useAppDispatch } from 'state/hooks';
 import { selectAllTokensByChainId } from 'state/reducers/lists';
 import { selectTokenOne, selectTokenTwo, setTokenOne, setTokenTwo } from 'state/reducers/selectedTokens';
 import { ExtendedNextPage } from 'types/ExtendedNextPage';
+import { Switch as HeadlessSwitch } from '@headlessui/react';
+import { selectMomijiUsage, setMomijiUsage } from 'state/reducers/swap';
 
 const swapType = SwapTypes.SwapExactIn;
 
@@ -47,6 +49,8 @@ const SwapIndexPage: ExtendedNextPage = () => {
 	const [activeToken, setActiveToken] = useState<SwapTokenNumber>(SwapTokenNumber.IN);
 	const tokenOne = useSelector(selectTokenOne);
 	const tokenTwo = useSelector(selectTokenTwo);
+
+	const momijiEnabled = useSelector(selectMomijiUsage);
 
 	const setTokenHandler = (token: TokenInfo, tokenNum: number) => {
 		if (tokenNum === SwapTokenNumber.IN) {
@@ -135,7 +139,7 @@ const SwapIndexPage: ExtendedNextPage = () => {
 
 									{props.values.info && (
 										<>
-											<SwapCardTradeRoute />
+											{momijiEnabled ? null : <SwapCardTradeRoute />}
 											<If
 												condition={
 													!(props.values.info.swapAmount.lte(BigNumber.from(0)) || props.values.info.swaps.length === 0)
@@ -184,6 +188,27 @@ const SwapIndexPage: ExtendedNextPage = () => {
 											</If>
 										</>
 									)}
+
+									<div className="flex items-center gap-2 pt-2">
+										<HeadlessSwitch
+											checked={momijiEnabled}
+											onChange={(e) => dispatch(setMomijiUsage(e))}
+											className={`${
+												momijiEnabled ? 'bg-lights-400' : 'bg-darks-200'
+											} relative inline-flex h-[2.4rem] w-[4.6rem] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+										>
+											<span className="sr-only">Use Momiji</span>
+											<span
+												aria-hidden="true"
+												className={`${
+													momijiEnabled ? 'translate-x-9' : 'translate-x-0'
+												} pointer-events-none inline-block h-[2.125rem] w-[2.125rem] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+											/>
+										</HeadlessSwitch>
+										<span className={`${momijiEnabled ? 'text-lights-400' : 'text-darks-200'} inline-block align-middle text-sm`}>
+											{momijiEnabled ? 'Trade gasless by signature' : 'Trade with a gas fee'}
+										</span>
+									</div>
 								</div>
 							</Form>
 						)}
