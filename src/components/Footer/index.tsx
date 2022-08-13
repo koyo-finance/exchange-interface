4621562ec483513de1644965601a0ec76778339b
p7m <perpetuum7@protonmail.com>
2022-08-13 23:47:52 +0200

    refactor: remove unused fontawesome packs

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -34,22 +34,6 @@ const Footer: React.FC = () => {
 								Discord
 							</a>
 						</div>
-						{/* <div className="md:place-self-center md:justify-self-end">
-							<div className="flex gap-4 text-4xl">
-								<a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer">
-									@ts-expect-error This is quite odd
-									<FontAwesomeIcon icon={faTwitter} />
-								</a>
-								<a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer">
-									@ts-expect-error This is quite odd
-									<FontAwesomeIcon icon={faGithub} />
-								</a>
-								<a href={DISCORD_LINK} target="_blank" rel="noopener noreferrer">
-									@ts-expect-error This is quite odd
-									<FontAwesomeIcon icon={faDiscord} />
-								</a>
-							</div>
-						</div>  */}
 					</div>
 				</div>
 			</div>
