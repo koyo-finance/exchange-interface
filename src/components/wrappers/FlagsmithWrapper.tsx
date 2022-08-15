fb80018f7fdf3bfaa258be986c51f410da9c7a0c
p7m <perpetuum7@protonmail.com>
2022-08-15 20:14:59 +0200

    feat: identify user to flagsmith

--- src/components/wrappers/FlagsmithWrapper.tsx
+++ src/components/wrappers/FlagsmithWrapper.tsx
@@ -1,16 +1,27 @@
 import { config } from 'core/config';
+import { getAddress } from 'ethers/lib/utils';
 import flagsmith from 'flagsmith/isomorphic';
 import { FlagsmithProvider } from 'flagsmith/react';
-import React from 'react';
+import { useWeb3 } from 'hooks/useWeb3';
+import React, { useEffect } from 'react';
 
 const FlagsmithWrapper: React.FC = ({ children }) => {
-	if (!config.flagsmithEnvironmentId) return <>{children}</>;
+	const { accountAddress } = useWeb3();
+	const { flagsmithEnvironmentId } = config;
+
+	useEffect(() => {
+		if (!flagsmithEnvironmentId || !accountAddress) return;
+
+		void flagsmith.identify(getAddress(accountAddress));
+	}, [accountAddress, flagsmithEnvironmentId]);
+
+	if (!flagsmithEnvironmentId) return <>{children}</>;
 
 	return (
 		<FlagsmithProvider
 			flagsmith={flagsmith}
 			options={{
-				environmentID: config.flagsmithEnvironmentId
+				environmentID: flagsmithEnvironmentId
 			}}
 		>
 			<>{children}</>
