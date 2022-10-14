37b183cf5fca4f23a2ed793f7c35353c3778bbbe
p7m <perpetuum7@protonmail.com>
2022-10-14 21:36:04 +0200

    alchemy badge

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,7 +11,7 @@ const Footer: React.FC = () => {
 							{/* eslint-disable-next-line @next/next/no-img-element */}
 							<img src="wide-dark.svg" alt="Koyo wide logo" className="max-h-12" />
 						</div>
-						<div className=" text-lg font-semibold text-white">
+						<div className="text-lg font-semibold text-white">
 							<span className=" text-gray-500">Developers</span>
 							{/* eslint-disable-next-line prettier/prettier */}
 							<a href={DOCS_LINK} className="link link-hover" target="_blank" rel="noreferrer">
@@ -34,6 +34,18 @@ const Footer: React.FC = () => {
 								Discord
 							</a>
 						</div>
+					</div>
+					<hr className="rounded-xl" />
+					<div className="footer flex justify-end px-6 pt-2">
+						<a href="https://alchemy.com/?r=jYzNzgwNzE1NjgyN" target="_blank" rel="noreferrer">
+							{/* eslint-disable-next-line @next/next/no-img-element */}
+							<img
+								id="badge-button"
+								className="btn p-0"
+								src="https://static.alchemyapi.io/images/marketing/badgeLight.png"
+								alt="Alchemy Supercharged"
+							/>
+						</a>
 					</div>
 				</div>
 			</div>
